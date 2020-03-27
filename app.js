f1892cd068c730f6f42e803d859e8c96fd0e3104
trevor-ofarrell <744@holbertonschool.com>
2020-03-27 15:45:54 -0700

    nothing major, just styling and tweaking things

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.post('/', (req, res) => {
           const browser = await puppeteer.launch({
             executablePath: '/usr/bin/google-chrome-stable',
             args: [
+              '--incognito',
               '--no-sandbox',
               '--disable-setuid-sandbox',
               '--disable-infobars',
@@ -35,13 +36,13 @@ app.post('/', (req, res) => {
               '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"'
             ],
             headless: false,
-        });
-    
-        const page = await browser.newPage()
+        })
+        const context = await browser.createIncognitoBrowserContext();
+        const page = await context.newPage()
         await page.goto('https://www.instagram.com/')
         await page.setViewport({
-          width: 1200,
-          height: 800
+          width: 800,
+          height: 500
         });
         await page.waitForSelector(
           '#react-root > section > main > article > div.rgFsT > div:nth-child(1) > div > form > div:nth-child(2) > div > label > input',
@@ -50,19 +51,23 @@ app.post('/', (req, res) => {
         await page.type('#react-root > section > main > article > div.rgFsT > div:nth-child(1) > div > form > div:nth-child(2) > div > label > input', email)
         await page.waitForSelector('div.-MzZI:nth-child(3) > div:nth-child(1) > label:nth-child(1) > input:nth-child(2)', { timeout: 9000 })
         await page.type('div.-MzZI:nth-child(3) > div:nth-child(1) > label:nth-child(1) > input:nth-child(2)', password)
+        console.log('logged in')
         try {
           await page.waitForSelector('.L3NKy > div:nth-child(1)', {timeout: 4000})
         } catch { }
         await page.click('.L3NKy > div:nth-child(1)')
         await page.waitForSelector('#react-root > section > nav > div._8MQSO.Cx7Bp > div > div > div.oJZym > a > div > div > img',  { timeout: 9000 })
         await page.goto('https://www.instagram.com/trevorthegnar/')
+        console.log('at profile')
         await page.click('#react-root > section > main > div > header > section > ul > li:nth-child(2) > a')
         try {
           await page.waitForSelector('body > div.RnEpo.Yx5HN > div > div.isgrP > ul > div > li',  { timeout: 6000 })
         } catch {
+            console.log('first failure')
             await page.reload({ waitUntil: ["networkidle0", "domcontentloaded"] });
             await page.click('#react-root > section > main > div > header > section > ul > li:nth-child(2) > a')
             await page.waitForSelector('body > div.RnEpo.Yx5HN > div > div.isgrP > ul > div > li',  { timeout: 9000 })
+            console.log('made it')
           }
 
         const extractFollowers = () => {
